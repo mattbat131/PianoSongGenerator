58d0eee4de30af734aa507486c754eeab91167d1
mattbat131 <sternm@bxsicence.edu>
2015-12-12 18:37:16 -0500

    :(

--- src/main.py
+++ src/main.py
@@ -3,11 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.dialects import sqlite
-import mingus
-import fluidsynth
+import pygame
+import pygame.midi
+from time import sleep
 import pickle
 
 Base = declarative_base()
+note_list = [27.5, 29.1352, 30.8677, ]
+
 
 class Songs(Base):
     __tablename__ = 'music'
@@ -33,9 +36,9 @@ class Beat:
         self.beat = list()
         for i in range(64):
             #generate beat
-            play_sound = 0
-            amplitude = 0
-            accent = 0
+            play_sound = 1
+            amplitude = 100
+            accent = 2
             self.beat.append((play_sound, amplitude, accent))
 
     def get_beat():
@@ -55,17 +58,25 @@ class Sound():
         return self.sound
 
 def play_sound(sound):
+    pygame.init()
+    pygame.midi.init()
+
+    port = pygame.midi.get_default_output_id()
+    midi_out = pygame.midi.Output(port, 0)
+    midi_out.set_instrument(0)
+
     for n in sound.sound:
         b = n[3]
         if b[0]:
-            play = Note('[0]-[1]'.format(n.note, n.key))
-            play.channel = n.octave
-            play.velocity = b.amplitude
-            fluidsynth.play_Note(play)
+            #play = Note('[0]-[1]'.format(n.note, n.key))
+            midinum = n[2]
+            velocity = b[3]
+            midi_out.note_on(72, 127)
+            sleep(.5)
+            midi_out.note_off(72, 127)
 
 def get_rank():
     rank = 0
-    print("GETTING RANK")
     return rank
 
 def main():
@@ -77,6 +88,8 @@ def main():
     session = Session()
     session.add(song)
     session.commit()
+    for u in session.query(Songs):
+        print(u)
 
 if __name__ == "__main__":
     main()
