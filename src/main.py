90f147cf64c5929635e3c97ee34281a73fe31dc9
mattbat131 <sternm@bxsicence.edu>
2015-12-12 16:01:34 -0500

    update

--- src/main.py
+++ src/main.py
@@ -1,14 +1,30 @@
 import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, create_engine
+import mingus
+import fluidsynth
+
+Base = declarative_base()
 
 class Songs(Base):
     __tablename__ = 'music'
 
     id = Column(Integer, primary_key=True)
     rank = Column(Integer)
-    note[64] = Column()
+    song = Column(String)
+
+    def __init__(self, rank, song):
+        self.rank = rank
+        self.song = song
 
     def __repr__(self):
-        return "<Song note='%s', rank='%s')>" % ([note for note in self.note], self.rank)
+        return "<(song='%s', rank='%s')>" % (self.song, self.rank)
+
+engine = create_engine('sqlite:///songs.db')
+Session = sessionmaker()
+Session.configure(bind=engine)
+Base.metadata.create_all(engine)
 
 class Beat:
     def __init__(self):
@@ -18,26 +34,47 @@ class Beat:
             play_sound = 0
             amplitude = 0
             accent = 0
-            self.beat.append(zip(play_sound, amplitude, accent))
+            self.beat.append((play_sound, amplitude, accent))
 
     def get_beat():
         return self.beat
 
-class Sound(Beat):
-    def __init__(self, note, type, octave):
+class Sound():
+    def __init__(self, b):
         self.sound = list()
-        for beat in self.beat:
+        for beat in b.beat:
             #generate notes
-            key = 0
+            key = '#'
             octave = 0
-            note = 0
-            self.sound.append(zip(note, key, octave, beat))
+            note = 'A'
+            self.sound.append((note, key, octave, beat))
 
     def get_sound():
         return self.sound
 
+def play_sound(sound):
+    for n in sound.sound:
+        b = n[3]
+        if b[0]:
+            play = Note('[0]-[1]'.format(n.note, n.key))
+            play.channel = n.octave
+            play.velocity = b.amplitude
+            fluidsynth.play_Note(play)
+
+def get_rank():
+    rank = 0
+    print("GETTING RANK")
+    return rank
+
 def main():
-    print("running")
+    beat = Beat()
+    sound = Sound(beat)
+    play_sound(sound)
+    rank = get_rank()
+    song = Songs(rank, sound)
+    session = Session()
+    session.add(song)
+    session.commit()
 
 if __name__ == "__main__":
     main()
