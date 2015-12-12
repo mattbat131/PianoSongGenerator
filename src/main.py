c4401e78e2a0fe0b3b7c540bb043b2d526421e6e
mattbat131 <sternm@bxsicence.edu>
2015-12-12 13:34:50 -0500

    Update

--- src/main.py
+++ src/main.py
@@ -10,6 +10,32 @@ class Songs(Base):
     def __repr__(self):
         return "<Song note='%s', rank='%s')>" % ([note for note in self.note], self.rank)
 
+class Beat:
+    def __init__(self):
+        self.beat = list()
+        for i in range(64):
+            #generate beat
+            play_sound = 0
+            amplitude = 0
+            accent = 0
+            self.beat.append(zip(play_sound, amplitude, accent))
+
+    def get_beat():
+        return self.beat
+
+class Sound(Beat):
+    def __init__(self, note, type, octave):
+        self.sound = list()
+        for beat in self.beat:
+            #generate notes
+            key = 0
+            octave = 0
+            note = 0
+            self.sound.append(zip(note, key, octave, beat))
+
+    def get_sound():
+        return self.sound
+
 def main():
     print("running")
 
