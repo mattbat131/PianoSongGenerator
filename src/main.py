311c43f4974b7503494394ff8b67a3c6251aa777
Christina Ford <Christina Ford>
2015-12-12 20:56:56 -0500

    Finish pattern recognition

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
 import sqlalchemy
+from sqlalchemy import func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.dialects import sqlite
-import pygame
-import pygame.midi
-from time import sleep
-import pickle
+try:
+    import cPickle
+except:
+    import pickle as cPickle
+import math
+import struct
+import pyaudio
+from random import randint
+import random
+import codecs
+
 
 Base = declarative_base()
-note_list = [27.5, 29.1352, 30.8677, ]
-
 
 class Songs(Base):
     __tablename__ = 'music'
@@ -32,64 +37,94 @@ Session.configure(bind=engine)
 Base.metadata.create_all(engine)
 
 class Beat:
-    def __init__(self):
+    def __init__(self, beats):
         self.beat = list()
         for i in range(64):
             #generate beat
-            play_sound = 1
-            amplitude = 100
-            accent = 2
-            self.beat.append((play_sound, amplitude, accent))
+            play_sound = randint(0, 1)
+            amplitude = randint(1, 100)
+            duration = random.uniform(.01, 1)
+            self.beat.append((play_sound, amplitude, duration))
+
 
     def get_beat():
         return self.beat
 
 class Sound():
-    def __init__(self, b):
+    def __init__(self, b, notes):
         self.sound = list()
         for beat in b.beat:
             #generate notes
-            key = '#'
-            octave = 0
-            note = 'A'
-            self.sound.append((note, key, octave, beat))
+            frequency = randint(1000, 5000)
+            self.sound.append((frequency, beat))
+
 
     def get_sound():
         return self.sound
 
-def play_sound(sound):
-    pygame.init()
-    pygame.midi.init()
 
-    port = pygame.midi.get_default_output_id()
-    midi_out = pygame.midi.Output(port, 0)
-    midi_out.set_instrument(0)
+fs = 48000
+p = pyaudio.PyAudio()
+stream = p.open(
+    format=pyaudio.paFloat32,
+    channels=1,
+    rate=fs,
+    output=True)
 
-    for n in sound.sound:
-        b = n[3]
-        if b[0]:
-            #play = Note('[0]-[1]'.format(n.note, n.key))
-            midinum = n[2]
-            velocity = b[3]
-            midi_out.note_on(72, 127)
-            sleep(.5)
-            midi_out.note_off(72, 127)
+
+def get_beat_pattern(beats, ranks):
+    return 0
+
+
+def get_sound_pattern(notes, ranks):
+    return 0
+
+
+def play_tone(frequency, amplitude, duration, fs, stream):
+    N = int(fs / frequency)
+    T = int(frequency * duration)  # repeat for T cycles
+    dt = 1.0 / fs
+    # 1 cycle
+    tone = (amplitude * math.sin(2 * math.pi * frequency * n * dt)
+            for n in xrange(N))
+    # todo: get the format from the stream; this assumes Float32
+    data = ''.join(struct.pack('f', samp) for samp in tone)
+    for n in xrange(T):
+        stream.write(data)
+
 
 def get_rank():
-    rank = 0
+    rank = input("You like it???? [0-10] plz:")
     return rank
 
+
 def main():
-    beat = Beat()
-    sound = Sound(beat)
-    play_sound(sound)
-    rank = get_rank()
-    song = Songs(rank, pickle.dumps(sound))
     session = Session()
+    beat_pattern = [0]
+    note_pattern = [0]
+    
+    if session.query(Songs).first() is not None:
+        info = list()
+        ranks = list()
+        for x in session.query(Songs):
+            info.append(x.song)
+            ranks.append(x.rank)
+            
+        beat_pattern = get_beat_pattern([list(cPickle.loads(b))[1] for b in info], ranks)
+        note_pattern = get_sound_pattern([list(cPickle.loads(n))[0] for n in info], ranks)
+        
+    beat = Beat(beat_pattern)
+    sound = Sound(beat, note_pattern)
+    #for note in sound.sound:
+     #   if note[1][0] == 1:
+      #      play_tone(note[0], note[1][1], note[1][2], fs, stream)
+    rank = get_rank()
+    song = Songs(rank, cPickle.dumps(zip(sound.sound)))
     session.add(song)
     session.commit()
     for u in session.query(Songs):
-        print(u)
+        print("Rank: ", u.rank, "Song: ", list(cPickle.loads(u.song)))
+    
 
 if __name__ == "__main__":
     main()
